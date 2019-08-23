2270569ca0a403c08662a051425686bc61be9d26
Chris O'Hara <crsohara@gmail.com>
2019-08-23 19:35:43 +0300

    Part4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,7 @@ const dummy = (blogs) => {
   return 1
 }
 
-const totalLikes = (blogs)  => {
+const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
     return sum + item.likes
   }
@@ -12,7 +12,18 @@ const totalLikes = (blogs)  => {
     : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (prev, current) => {
+    return (prev.likes > current.likes) ? prev : current
+  }
+
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
