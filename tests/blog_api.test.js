69110541acd29e87ad3223a6479f9a79e636a857
Chris O'Hara <crsohara@gmail.com>
2019-08-27 22:55:29 +0300

    Wrap blog_api tests in describe

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -18,134 +18,150 @@ beforeEach( async () => {
 
 })
 
-test('blogs are returned as json', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('Get blogs:', () => {
+  test('Blogs are returned as json', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-  expect(response.body.length).toBe(helper.initialBlogs.length)
+    expect(response.body.length).toBe(helper.initialBlogs.length)
 
-})
-
-test('blogs unique identifier is named id', async () => {
-
-  const blogsInDb = await helper.blogsInDb()
-
-  blogsInDb.map( blog => expect(blog.id).toBeDefined() )
-
+  })
 })
 
 
-test('blog is created successfully', async() => {
+describe('Create blog:', () => {
+  test('Create a new blog entry', async() => {
 
-  const newBlog = {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  }
+    const newBlog = {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-type', /application\/json/)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-type', /application\/json/)
 
-  const blogsInDb = await helper.blogsInDb()
+    const blogsInDb = await helper.blogsInDb()
 
-  expect(blogsInDb.length === helper.initialBlogs.length + 1)
+    expect(blogsInDb.length === helper.initialBlogs.length + 1)
 
-  const contents = blogsInDb.map(blog => blog.title)
+    const contents = blogsInDb.map(blog => blog.title)
 
-  expect(contents).toContain('Canonical string reduction')
+    expect(contents).toContain('Canonical string reduction')
 
+  })
 })
 
-test('blog without likes property sets likes to 0', async () => {
 
-  const newBlog =  {
-    title: 'React patterns',
-    author: 'James Chan',
-    url: 'https://reactpatterns.com/',
-  }
+describe('Blog attributes:', () => {
+  test('Blogs have unique identifier is named id', async () => {
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-type', /application\/json/)
+    const blogsInDb = await helper.blogsInDb()
 
-  const blogsInDb = await helper.blogsInDb()
+    blogsInDb.map( blog => expect(blog.id).toBeDefined() )
 
-  expect(blogsInDb.length === helper.initialBlogs.length + 1)
+  })
 
-  blogsInDb.map( blog => expect(blog.likes).toBeGreaterThanOrEqual(0) )
 
+  test('Blogs without no "likes" attribute sets likes to 0', async () => {
+
+    const newBlog =  {
+      title: 'React patterns',
+      author: 'James Chan',
+      url: 'https://reactpatterns.com/',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-type', /application\/json/)
+
+    const blogsInDb = await helper.blogsInDb()
+
+    expect(blogsInDb.length === helper.initialBlogs.length + 1)
+
+    blogsInDb.map( blog => expect(blog.likes).toBeGreaterThanOrEqual(0) )
+
+  })
+
+
+  test('New blog post without title or url returns status 400', async () => {
+
+    const newBlog =  {
+      author: 'James Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 1
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+  })
 })
 
-test('blog POSTed without title or url returns status 400', async () => {
+describe('Delete blog:', () => {
 
-  const newBlog =  {
-    author: 'James Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 1
-  }
+  test('Delete a blog', async () => {
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+    const blogsInDb = await helper.blogsInDb()
 
+    const blogToDelete = blogsInDb[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsInDbAfterDeletion = await helper.blogsInDb()
+
+    expect(blogsInDbAfterDeletion.length).toBe(helper.initialBlogs.length - 1)
+
+    const contents = blogsInDbAfterDeletion.map(blog => blog.id)
+
+    expect(contents).not.toContain(blogToDelete.id)
+
+  })
 })
 
-test('delete a blog', async () => {
 
-  const blogsInDb = await helper.blogsInDb()
+describe('Update blog:', () => {
 
-  const blogToDelete = blogsInDb[0]
+  test('Update a blog', async () => {
 
-  await api
-    .delete(`/api/blogs/${blogToDelete.id}`)
-    .expect(204)
+    const blogsInDb = await helper.blogsInDb()
 
-  const blogsInDbAfterDeletion = await helper.blogsInDb()
+    const blogToUpdate = blogsInDb[0]
 
-  expect(blogsInDbAfterDeletion.length).toBe(helper.initialBlogs.length - 1)
+    const updatedBlogData = {
+      title: 'my updated title!'
+    }
 
-  const contents = blogsInDbAfterDeletion.map(blog => blog.id)
+    const updatedBlog = Object.assign(blogToUpdate, updatedBlogData)
 
-  expect(contents).not.toContain(blogToDelete.id)
+    const response = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedBlog)
+      .expect(200)
 
-})
+    expect(response.body.title).toContain(updatedBlogData.title)
 
-test('update a blog', async () => {
+    const blogsInDbAfterUpdate = await helper.blogsInDb()
 
-  const blogsInDb = await helper.blogsInDb()
+    const titles = blogsInDbAfterUpdate.map( blog => blog.title)
 
-  const blogToUpdate = blogsInDb[0]
+    expect(titles).toContain(updatedBlogData.title)
 
-  const updatedBlogData = {
-    title: 'my updated title!'
-  }
-
-  const updatedBlog = Object.assign(blogToUpdate, updatedBlogData)
-
-  const response = await api
-    .put(`/api/blogs/${blogToUpdate.id}`)
-    .send(updatedBlog)
-    .expect(200)
-
-  expect(response.body.title).toContain(updatedBlogData.title)
-
-  const blogsInDbAfterUpdate = await helper.blogsInDb()
-
-  const titles = blogsInDbAfterUpdate.map( blog => blog.title)
-
-  expect(titles).toContain(updatedBlogData.title)
-
+  })
 })
 
 
