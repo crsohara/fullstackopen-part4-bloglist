4b93feceb57cfa857b481c6eb8c5664ebe39acc7
Chris O'Hara <crsohara@gmail.com>
2019-08-27 21:39:16 +0300

    Part4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -37,6 +37,34 @@ test('blogs unique identifier is named id', async () => {
 })
 
 
+test('blog is created successfully', async() => {
+
+  const newBlog = {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-type', /application\/json/)
+
+  const blogsInDb = await helper.blogsInDb()
+
+  expect(blogsInDb.length === helper.initialBlogs.length + 1)
+
+  const contents = blogsInDb.map(blog => blog.title)
+
+  expect(contents).toContain('Canonical string reduction')
+
+})
+
+
 afterAll( async () => {
   await Blog.deleteMany({})
   mongoose.connection.close()
